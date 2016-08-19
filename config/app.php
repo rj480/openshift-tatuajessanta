ee13c044f88fca75e80a41e6c567fcb228df3b43
roberto jose arena mazuera <rjam480@gmail.com>
2016-08-19 17:14:23 -0500

    app cambio commit

--- config/app.php
+++ config/app.php
@@ -78,9 +78,9 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'MCRYPT_RIJNDAEL_128'),
+    'key' => env('APP_KEY', 'SomeRandomString'),
 
-    'cipher' => 'AES-256-CBC',
+    'cipher' => 'AES-128-CBC',
 
     /*
     |--------------------------------------------------------------------------
